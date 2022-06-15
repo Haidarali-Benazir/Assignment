2a2f2183e107c01539777719aa53996e2959d3a8
HaidaraliChamcham <haideralichamcham98@gmail.com>
2022-06-15 20:41:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './App.css';
+import MaterialTable from 'material-table';
+
+function App() {
+  return (
+   <div>
+    hi my girl
+    <MaterialTable
+      title="Basic Export Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      options={{
+        exportButton: true
+      }}
+    />
+   </div>
+  );
+}
+
+export default App;
